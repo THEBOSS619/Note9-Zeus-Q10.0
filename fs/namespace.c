478570b04a9a93c8b39faed514af57c6e4dbeb2a
arter97 <qkrwngud825@gmail.com>
2013-12-17 02:23:46 +0900

    afsync: notify when afsync got enabled
    Conflicts:
    	fs/namespace.c

--- fs/namespace.c
+++ fs/namespace.c
@@ -3077,8 +3077,10 @@ static int do_new_mount(struct path *path, const char *fstype, int flags,
 	if (!err && ((!strcmp(fstype, "ext4") &&
 	    !strcmp(path->dentry->d_name.name, "data")) ||
 	    (!strcmp(fstype, "fuse") &&
-	    !strcmp(path->dentry->d_name.name, "emulated"))))
+	    !strcmp(path->dentry->d_name.name, "emulated")))) {
                 mnt->mnt_sb->fsync_flags |= FLAG_ASYNC_FSYNC;
+		pr_info("namespace: enabled FLAG_ASYNC_FSYNC\n");
+	}
 #endif
 	return err;
 }
