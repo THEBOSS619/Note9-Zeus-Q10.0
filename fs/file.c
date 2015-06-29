2e61d00796aee062865c877759db51a64f39be64
Eric Dumazet <edumazet@google.com>
2015-06-29 17:10:30 +0200

    fs/file.c: __fget() and dup2() atomicity rules
    __fget() does lockless fetch of pointer from the descriptor
    table, attempts to grab a reference and treats "it was already
    zero" as "it's already gone from the table, we just hadn't
    seen the store, let's fail".  Unfortunately, that breaks the
    atomicity of dup2() - __fget() might see the old pointer,
    notice that it's been already dropped and treat that as
    "it's closed".  What we should be getting is either the
    old file or new one, depending whether we come before or after
    dup2().
    
    Dmitry had following test failing sometimes :
    
    int fd;
    void *Thread(void *x) {
      char buf;
      int n = read(fd, &buf, 1);
      if (n != 1)
        exit(printf("read failed: n=%d errno=%d\n", n, errno));
      return 0;
    }
    
    int main()
    {
      fd = open("/dev/urandom", O_RDONLY);
      int fd2 = open("/dev/urandom", O_RDONLY);
      if (fd == -1 || fd2 == -1)
        exit(printf("open failed\n"));
      pthread_t th;
      pthread_create(&th, 0, Thread, 0);
      if (dup2(fd2, fd) == -1)
        exit(printf("dup2 failed\n"));
      pthread_join(th, 0);
      if (close(fd) == -1)
        exit(printf("close failed\n"));
      if (close(fd2) == -1)
        exit(printf("close failed\n"));
      printf("DONE\n");
      return 0;
    }
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Reported-by: Dmitry Vyukov <dvyukov@google.com>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- fs/file.c
+++ fs/file.c
@@ -176,9 +176,7 @@ static int expand_fdtable(struct files_struct *files, unsigned int nr)
 	 * or have finished their rcu_read_lock_sched() section.
 	 */
 	if (atomic_read(&files->count) > 1) {
-		rcu_expedite_gp();
 		synchronize_sched();
-		rcu_unexpedite_gp();
 	}
 
 	spin_lock(&files->file_lock);
