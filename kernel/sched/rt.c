b2bd6861680a8ea2caa14ca2d173c3458703d8d4
Joel Fernandes <joelaf@google.com>
2017-04-27 01:29:00 -0700

    marlin: ANDROID: sched: rt: Avoid preempting CPUs if softirq explicity disable
    In task_may_not_preempt, its useful to check for SOFTIRQ_DISABLE_OFFSET
    which is set only when softirq is explicity disabled, as opposed to
    SOFTIRQ_OFFSET which is set when SOFTIRQs are being processed.
    
    With this, I don't hit any more scheduling issues with the WiFi driver.
    
    Bug: 37562902
    
    Change-Id: I3a7556f699bbed255b58d5c4e16136e7acb172a6
    Signed-off-by: Joel Fernandes <joelaf@google.com>
    [Slight modifications to work with existing code]
    Signed-off-by: joshuous <joshuous@gmail.com>
    
     Conflicts:
    	kernel/sched/rt.c

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -1881,6 +1881,16 @@ static void schedtune_dequeue_rt(struct rq *rq, struct task_struct *p)
 }
 
 /*
+ * Determine if destination CPU explicity disable softirqs,
+ * this is different from CPUs which are running softirqs.
+ * pc is the preempt count to check.
+ */
+static bool softirq_masked(int pc)
+{
+	return !!((pc & SOFTIRQ_MASK)>= SOFTIRQ_DISABLE_OFFSET);
+}
+
+/*
  * Return whether the given cpu has (or will shortly have) an RT task
  * ready to run. NB: This is a heuristic and is subject to races.
  */
@@ -2006,11 +2016,17 @@ task_may_not_preempt(struct task_struct *task, int cpu)
 	__u32 softirqs = per_cpu(active_softirqs, cpu) |
 			 __IRQ_STAT(cpu, __softirq_pending);
 	struct task_struct *cpu_ksoftirqd = per_cpu(ksoftirqd, cpu);
+	int task_pc = 0;
+
+	if (task)
+		task_pc = task_preempt_count(task);
+
+	if (softirq_masked(task_pc))
+		return true;
 
 	return ((softirqs & LONG_SOFTIRQ_MASK) &&
 		(task == cpu_ksoftirqd || is_idle_task(task) ||
-		 (task_thread_info(task)->preempt_count
-		     & (HARDIRQ_MASK | SOFTIRQ_MASK))));
+		  (task_pc & (HARDIRQ_MASK | SOFTIRQ_MASK))));
 }
 
 #ifdef CONFIG_RT_GROUP_SCHED
