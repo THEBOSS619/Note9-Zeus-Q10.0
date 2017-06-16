a54be0a85100fdbb773dffa36aaeb2974618a818
Joonwoo Park <joonwoop@codeaurora.org>
2017-06-16 11:58:58 -0700

    sched: introduce sched_{up,down}migrate knobs
    Introduce knobs sched_upmigrate and sched_downmigrate to
    control thresholds to up and down migrate tasks between clusters at a
    runtime for ease of power and performance tuning.
    
    Change-Id: I17a464388b404b61d9a9dbe1beee4883c4db8e3f
    Signed-off-by: Joonwoo Park <joonwoop@codeaurora.org>
    [satyap@codeaurora.org: resolve merge conflicts & update
    functinality as needed on msm-4.14]
    Signed-off-by: Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -157,6 +157,9 @@ unsigned int sysctl_sched_cfs_bandwidth_slice		= 5000UL;
  */
 unsigned int capacity_margin				= 1280;
 
+unsigned int sysctl_sched_capacity_margin_up = 1078; /* ~5% margin */
+unsigned int sysctl_sched_capacity_margin_down = 1205; /* ~15% margin */
+
 static unsigned int __maybe_unused sched_small_task_threshold = 102;
 
 static inline void update_load_add(struct load_weight *lw, unsigned long inc)
@@ -3829,9 +3832,18 @@ util_est_dequeue(struct cfs_rq *cfs_rq, struct task_struct *p, bool task_sleep)
 	WRITE_ONCE(p->se.avg.util_est, ue);
 }
 
-static inline int task_fits_capacity(struct task_struct *p, long capacity)
+static inline int task_fits_capacity(struct task_struct *p,
+					long capacity,
+					int cpu)
 {
-	return capacity * 1024 > task_util_est(p) * capacity_margin;
+	unsigned int margin;
+
+	if (capacity_orig_of(task_cpu(p)) > capacity_orig_of(cpu))
+		margin = sysctl_sched_capacity_margin_down;
+	else
+		margin = sysctl_sched_capacity_margin_up;
+
+	return capacity * 1024 > task_util_est(p) * margin;
 }
 
 static inline void update_misfit_status(struct task_struct *p, struct rq *rq)
@@ -3844,7 +3856,7 @@ static inline void update_misfit_status(struct task_struct *p, struct rq *rq)
 		return;
 	}
 
-	if (task_fits_capacity(p, capacity_of(cpu_of(rq)))) {
+	if (task_fits_capacity(p, capacity_of(cpu_of(rq))), rq->cpu) {
 		rq->misfit_task_load = 0;
 		return;
 	}
@@ -6717,7 +6729,7 @@ select_idle_capacity(struct task_struct *p, struct sched_domain *sd, int target)
 
 		if (!idle_cpu(cpu) && !sched_idle_cpu(cpu))
 			continue;
-		if (task_fits_capacity(p, cpu_cap))
+		if (task_fits_capacity(p, cpu_cap, cpu))
 			return cpu;
 
 		if (cpu_cap > best_cap) {
@@ -7379,7 +7391,7 @@ static int wake_cap(struct task_struct *p, int cpu, int prev_cpu)
 	/* Bring task utilization in sync with prev_cpu */
 	sync_entity_load_avg(&p->se);
 
-	return !task_fits_capacity(p, min_cap);
+	return !task_fits_capacity(p, min_cap, cpu);
 }
 
 static inline bool
@@ -8908,7 +8920,7 @@ static int detach_tasks(struct lb_env *env)
 
 		case migrate_misfit:
 			/* This is not a misfit task */
-			if (task_fits_capacity(p, capacity_of(env->src_cpu)))
+			if (task_fits_capacity(p, capacity_of(env->src_cpu)), env->src_cpu)
 				goto next;
 
 			env->imbalance = 0;
@@ -9899,7 +9911,7 @@ static inline void update_sg_wakeup_stats(struct sched_domain *sd,
 
 	/* Check if task fits in the group */
 	if (sd->flags & SD_ASYM_CPUCAPACITY &&
-	    !task_fits_capacity(p, group->sgc->max_capacity)) {
+	    !task_fits_capacity(p, group->sgc->max_capacity), cpu) {
 		sgs->group_misfit_task_load = 1;
 	}
 
