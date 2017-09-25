cec5b65a0af275a72730a6fd859e6101c895ff31
Youngtae Lee <yt0729.lee@samsung.com>
2017-09-25 06:20:35 -0700

    schedutil: change reference max frequency
    When cpuinfo.max_frequency and policy->max is different,
    This causes a bug in which the frequency increases quickly
    To solve this problem, always use fixed max_frequency(cpuinfo.max_frequency)
    
    Change-Id: I362268ebd5ffdb3e3229a414752cfb93651f2656
    Signed-off-by: Youngtae Lee <yt0729.lee@samsung.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -287,7 +287,7 @@ static unsigned int get_next_freq(struct sugov_policy *sg_policy,
 {
 	struct cpufreq_policy *policy = sg_policy->policy;
 	unsigned int freq = arch_scale_freq_invariant() ?
-				policy->cpuinfo.max_freq : policy->cur;
+				policy->max : policy->cur;
 
 	freq = map_util_freq(util, freq, max);
 
