e49c9bdf808fd87a975b77e63122ad178e8c99df
Jeff Layton <jlayton@redhat.com>
2018-01-09 08:21:39 -0500

    BACKPORT: ext4: convert to new i_version API
    Signed-off-by: Jeff Layton <jlayton@redhat.com>
    Acked-by: Theodore Ts'o <tytso@mit.edu>
    (cherry picked from commit ee73f9a52a34377887acfa3b76169709e80d577c)
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- fs/ext4/inline.c
+++ fs/ext4/inline.c
@@ -13,6 +13,7 @@
  */
 
 #include <linux/fiemap.h>
+#include <linux/iversion.h>
 
 #include "ext4_jbd2.h"
 #include "ext4.h"
@@ -1045,7 +1046,7 @@ static int ext4_add_dirent_to_inline(handle_t *handle,
 	 */
 	dir->i_mtime = dir->i_ctime = ext4_current_time(dir);
 	ext4_update_dx_flag(dir);
-	dir->i_version++;
+	inode_inc_iversion(dir);
 	ext4_mark_inode_dirty(handle, dir);
 	return 1;
 }
@@ -1498,7 +1499,7 @@ int ext4_read_inline_dir(struct file *file,
 	 * dirent right now.  Scan from the start of the inline
 	 * dir to make sure.
 	 */
-	if (file->f_version != inode->i_version) {
+	if (inode_cmp_iversion(inode, file->f_version)) {
 		for (i = 0; i < extra_size && i < offset;) {
 			/*
 			 * "." is with offset 0 and
@@ -1530,7 +1531,7 @@ int ext4_read_inline_dir(struct file *file,
 		}
 		offset = i;
 		ctx->pos = offset;
-		file->f_version = inode->i_version;
+		file->f_version = inode_query_iversion(inode);
 	}
 
 	while (ctx->pos < extra_size) {
