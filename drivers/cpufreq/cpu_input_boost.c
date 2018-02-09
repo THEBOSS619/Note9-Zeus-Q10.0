7e0c97b22620ac880f52011338326c1dfd0e5065
joshuous <joshuous@gmail.com>
2018-02-09 22:52:10 +0800

    cpu_input_boost: Implement Dynamic SchedTune Boost v3
    Dynamic SchedTune Boost occurs on-the-fly during interactions, such
    as touch input. This idea was conceived because of an existing battery
    drain issue whereby setting a permanent schedtune boost would cause
    boosting to occur even when the phone was idling. For example, if a user
    sets the /dev/stune/top-app/schedtune.boost to 10, the device may not
    idle at the lowest frequency step, which can lead to higher idle and
    active drain rates. Thus it should consume less power to boost only
    during interactions.
    
    v1 worked by changing the value returned by schedtune_cpu_boost(), but
    did not change the true boost value of the top-app cgroup. In comparison,
    v2 modifies the value in /dev/stune/top-app/schedtune.boost directly.
    v3 reworks the entire Dynamic SchedTune Boost code and improves detection
    of top-app or any other SchedTune group.
    
    The tunable can be found in /sys/module/cpu_boost/parameters/dynamic_stune_boost.
    
    Signed-off-by: joshuous <joshuous@gmail.com>
    [nc: Adapted from cpu-boost.c to cpu_input_boost.c]
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -36,6 +36,11 @@ module_param(input_boost_duration, short, 0644);
 #define MAX_BOOST		BIT(2)
 #define GENERAL_BOOST		BIT(3)
 
+#ifdef CONFIG_DYNAMIC_STUNE_BOOST
+static int dynamic_stune_boost;
+module_param(dynamic_stune_boost, uint, 0644);
+#endif /* CONFIG_DYNAMIC_STUNE_BOOST */
+
 struct boost_drv {
 	struct workqueue_struct *wq;
 	struct work_struct input_boost;
@@ -191,6 +196,11 @@ static void input_boost_worker(struct work_struct *work)
 		update_online_cpu_policy();
 	}
 
+#ifdef CONFIG_DYNAMIC_STUNE_BOOST
+	/* Set dynamic stune boost value */
+	do_stune_boost("top-app", dynamic_stune_boost);
+#endif /* CONFIG_DYNAMIC_STUNE_BOOST */
+
 	queue_delayed_work(b->wq, &b->input_unboost,
 		msecs_to_jiffies(input_boost_duration));
 }
@@ -201,6 +211,12 @@ static void input_unboost_worker(struct work_struct *work)
 		container_of(to_delayed_work(work), typeof(*b), input_unboost);
 
 	clear_boost_bit(b, INPUT_BOOST);
+
+#ifdef CONFIG_DYNAMIC_STUNE_BOOST
+	/* Reset dynamic stune boost value to the default value */
+	reset_stune_boost("top-app");
+#endif /* CONFIG_DYNAMIC_STUNE_BOOST */
+
 	update_online_cpu_policy();
 	cpu_input_boost_kick_general(64);
 }
@@ -214,6 +230,11 @@ static void max_boost_worker(struct work_struct *work)
 		update_online_cpu_policy();
 	}
 
+#ifdef CONFIG_DYNAMIC_STUNE_BOOST
+	/* Set dynamic stune boost value */
+	do_stune_boost("top-app", dynamic_stune_boost);
+#endif /* CONFIG_DYNAMIC_STUNE_BOOST */	
+
 	queue_delayed_work(b->wq, &b->max_unboost,
 		msecs_to_jiffies(atomic_read(&b->max_boost_dur)));
 }
@@ -224,7 +245,12 @@ static void max_unboost_worker(struct work_struct *work)
 		container_of(to_delayed_work(work), typeof(*b), max_unboost);
 
 	clear_boost_bit(b, MAX_BOOST);
-	
+
+#ifdef CONFIG_DYNAMIC_STUNE_BOOST
+	/* Reset dynamic stune boost value to the default value */
+	reset_stune_boost("top-app");
+#endif /* CONFIG_DYNAMIC_STUNE_BOOST */
+
 	update_online_cpu_policy();
 	cpu_input_boost_kick_general(64);
 }
@@ -238,6 +264,11 @@ static void general_boost_worker(struct work_struct *work)
 		update_online_cpu_policy();
 	}
 
+#ifdef CONFIG_DYNAMIC_STUNE_BOOST
+	/* Set dynamic stune boost value */
+	do_stune_boost("top-app", dynamic_stune_boost);
+#endif /* CONFIG_DYNAMIC_STUNE_BOOST */	
+
 	queue_delayed_work(b->wq, &b->general_unboost,
 		msecs_to_jiffies(atomic_read(&b->general_boost_dur)));
 }
@@ -248,6 +279,12 @@ static void general_unboost_worker(struct work_struct *work)
 		container_of(to_delayed_work(work), typeof(*b), general_unboost);
 
 	clear_boost_bit(b, GENERAL_BOOST);
+
+#ifdef CONFIG_DYNAMIC_STUNE_BOOST
+	/* Reset dynamic stune boost value to the default value */
+	reset_stune_boost("top-app");
+#endif /* CONFIG_DYNAMIC_STUNE_BOOST */
+
 	update_online_cpu_policy();
 }
 
@@ -357,6 +394,11 @@ free_handle:
 
 static void cpu_input_boost_input_disconnect(struct input_handle *handle)
 {
+#ifdef CONFIG_DYNAMIC_STUNE_BOOST
+	/* Reset dynamic stune boost value to the default value */
+	reset_stune_boost("top-app");
+#endif /* CONFIG_DYNAMIC_STUNE_BOOST */
+
 	input_close_device(handle);
 	input_unregister_handle(handle);
 	kfree(handle);
