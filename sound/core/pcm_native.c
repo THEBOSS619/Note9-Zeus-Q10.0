d6a2ab6602d7da738b5ba6763b8fd4f0a3976c3f
Takashi Iwai <tiwai@suse.de>
2018-03-13 11:18:57 +0100

    ALSA: pcm: Remove VLA usage
    A helper function used by snd_pcm_hw_refine() still keeps using VLA
    for timestamps of hw constraint rules that are non-fixed size.
    
    Let's replace the VLA with a simple kmalloc() array.
    
    Reference: https://lkml.org/lkml/2018/3/7/621
    Reviewed-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>
    Signed-off-by: Takashi Iwai <tiwai@suse.de>
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

--- sound/core/pcm_native.c
+++ sound/core/pcm_native.c
@@ -282,10 +282,10 @@ int snd_pcm_hw_refine(struct snd_pcm_substream *substream,
 	struct snd_interval *i = NULL;
 	struct snd_mask *m = NULL;
 	struct snd_pcm_hw_constraints *constrs = &substream->runtime->hw_constraints;
-	unsigned int rstamps[constrs->rules_num];
+	unsigned int *rstamps;
 	unsigned int vstamps[SNDRV_PCM_HW_PARAM_LAST_INTERVAL + 1];
 	unsigned int stamp = 2;
-	int changed, again;
+	int changed, again, err = 0;
 
 	params->info = 0;
 	params->fifo_size = 0;
@@ -347,8 +347,10 @@ int snd_pcm_hw_refine(struct snd_pcm_substream *substream,
 			return changed;
 	}
 
-	for (k = 0; k < constrs->rules_num; k++)
-		rstamps[k] = 0;
+	rstamps = kcalloc(constrs->rules_num, sizeof(unsigned int), GFP_KERNEL);
+	if (!rstamps)
+		return -ENOMEM;
+
 	for (k = 0; k <= SNDRV_PCM_HW_PARAM_LAST_INTERVAL; k++) 
 		vstamps[k] = (params->rmask & (1 << k)) ? 1 : 0;
 	do {
@@ -408,8 +410,10 @@ int snd_pcm_hw_refine(struct snd_pcm_substream *substream,
 				vstamps[r->var] = stamp;
 				again = 1;
 			}
-			if (changed < 0)
-				return changed;
+			if (changed < 0) {
+				err = changed;
+				goto out;
+			}
 			stamp++;
 		}
 	} while (again);
@@ -447,7 +451,10 @@ int snd_pcm_hw_refine(struct snd_pcm_substream *substream,
 		}
 	}
 	params->rmask = 0;
-	return 0;
+
+out:
+	kfree(rstamps);
+	return err;
 }
 
 EXPORT_SYMBOL(snd_pcm_hw_refine);
