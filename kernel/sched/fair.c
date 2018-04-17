df111b74e305e1ed9b3a548a537ddd5c05eba887
Andrei F <luxneb@gmail.com>
2018-04-17 15:13:15 +0200

    sched: fair: Express idle cost power as percentage of busy state
    This seems overall better to express static leakage power usage than the unrealistic small values for WFI.
    
    Device tree values are expressed as % in SCHED_CAPACITY_SCALE.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5954,8 +5954,11 @@ static int sched_group_energy(struct energy_env *eenv)
 				group_util = group_norm_util(eenv, sg);
 
 				sg_busy_energy = (group_util * sg->sge->cap_states[cap_idx].power);
-				sg_idle_energy = ((SCHED_CAPACITY_SCALE-group_util)
-								* sg->sge->idle_states[idle_idx].power);
+				sg_idle_energy = ((SCHED_CAPACITY_SCALE-group_util) 
+							* ((sg->sge->cap_states[cap_idx].power 
+								* sg->sge->idle_states[idle_idx].power)
+								>> SCHED_CAPACITY_SHIFT)
+							);
 
 				total_energy += sg_busy_energy + sg_idle_energy;
 
