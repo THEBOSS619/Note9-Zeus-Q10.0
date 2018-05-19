d7c0b23800f791a27fc14d163c412ffa0d1a0e68
kdrag0n <dragon@khronodragon.com>
2018-05-18 19:13:07 -0700

    printk: optimize disabled printk state
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -1917,6 +1917,9 @@ asmlinkage int vprintk_emit(int facility, int level,
 			    const char *dict, size_t dictlen,
 			    const char *fmt, va_list args)
 {
+	/* return instantly if printk is disabled */
+	if (likely(printk_mode == 0))
+		return 0;
 	static bool recursion_bug;
 	static char textbuf[LOG_LINE_MAX];
 	char *text = textbuf;
@@ -1930,10 +1933,6 @@ asmlinkage int vprintk_emit(int facility, int level,
 
 	/* cpu currently holding logbuf_lock in this function */
 	static unsigned int logbuf_cpu = UINT_MAX;
-
-	// if printk mode is disabled, terminate instantly
-	if (printk_mode == 0)
-			return 0;
 
 	if (level == LOGLEVEL_SCHED) {
 		level = LOGLEVEL_DEFAULT;
@@ -2066,9 +2065,9 @@ EXPORT_SYMBOL(vprintk_emit);
 
 asmlinkage int vprintk(const char *fmt, va_list args)
 {
-	// if printk mode is disabled, terminate instantly
-	if (printk_mode == 0)
-			return 0;
+	/* return instantly if printk is disabled */
+	if (likely(printk_mode == 0))
+		return 0;
 
 	return vprintk_emit(0, LOGLEVEL_DEFAULT, NULL, 0, fmt, args);
 }
@@ -2078,12 +2077,12 @@ asmlinkage int printk_emit(int facility, int level,
 			   const char *dict, size_t dictlen,
 			   const char *fmt, ...)
 {
+	/* return instantly if printk is disabled */
+	if (likely(printk_mode == 0))
+		return 0;
+
 	va_list args;
 	int r;
-
-	// if printk mode is disabled, terminate instantly
-	if (printk_mode == 0)
-			return 0;
 
 	va_start(args, fmt);
 	r = vprintk_emit(facility, level, dict, dictlen, fmt, args);
@@ -2095,11 +2094,11 @@ EXPORT_SYMBOL(printk_emit);
 
 int vprintk_default(const char *fmt, va_list args)
 {
-	int r;
+	/* return instantly if printk is disabled */
+	if (likely(printk_mode == 0))
+		return 0;
 
-	// if printk mode is disabled, terminate instantly
-	if (printk_mode == 0)
-			return 0;
+	int r;
 
 #ifdef CONFIG_KGDB_KDB
 	if (unlikely(kdb_trap_printk)) {
