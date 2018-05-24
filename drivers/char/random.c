c4a0bf37ba84634b3fce21be3ab768ba1e82da00
kdrag0n <dragon@khronodragon.com>
2018-05-23 17:39:44 -0700

    random: remove unnecessary checks
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- drivers/char/random.c
+++ drivers/char/random.c
@@ -750,12 +750,6 @@ static int credit_entropy_bits_safe(struct entropy_store *r, int nbits)
 {
 	const int nbits_max = r->poolinfo->poolwords * 32;
 
-	if (nbits < 0)
-		return -EINVAL;
-
-	if (nbits < 0)
-		return -EINVAL;
-
 	/* Cap the value to avoid overflows */
 	nbits = min(nbits,  nbits_max);
 
