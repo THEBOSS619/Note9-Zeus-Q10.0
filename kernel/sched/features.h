dfb43faae5f57516a4eb5a3177e3f87c26e97b7d
Andrei F <luxneb@gmail.com>
2018-05-25 18:53:39 +0200

    sched/fair: Migration hysteresis filter from HiSilicon EAS

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -118,6 +118,7 @@ SCHED_FEAT(EAS_PREFER_IDLE, true)
  * OFF: Use whichever of target or backup saves most.
  */
 SCHED_FEAT(FBT_STRICT_ORDER, true)
+SCHED_FEAT(HISI_FILTER, true)
 
 #ifdef CONFIG_SCHED_EHMP
 SCHED_FEAT(EXYNOS_HMP, true)
