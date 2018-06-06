188ea3f43f86277ff7eb8444e0669bdfb44f78c9
Alistair Strachan <astrachan@google.com>
2018-06-06 13:46:44 -0700

    Revert "proc: Convert proc_mount to use mount_ns."
    This reverts commit e94591d0d90c13166cb6eb54ce5f96ed13d81b55.
    
    This cleanup broke the parsing of procfs mount parameters.
    
    Bug: 79705088
    Change-Id: I0f07180ef9a994c884abfa269ffb273ee0bcbc0d
    Signed-off-by: Alistair Strachan <astrachan@google.com>

--- fs/proc/inode.c
+++ fs/proc/inode.c
@@ -457,7 +457,7 @@ struct inode *proc_get_inode(struct super_block *sb, struct proc_dir_entry *de)
 	return inode;
 }
 
-int proc_fill_super(struct super_block *s, void *data, int silent)
+int proc_fill_super(struct super_block *s)
 {
 	struct inode *root_inode;
 	int ret;
@@ -469,7 +469,7 @@ int proc_fill_super(struct super_block *s, void *data, int silent)
 #endif
 
 	/* User space would break if executables or devices appear on proc */
-	s->s_iflags |= SB_I_USERNS_VISIBLE | SB_I_NOEXEC | SB_I_NODEV;
+	s->s_iflags |= SB_I_USERNS_VISIBLE | SB_I_NODEV;
 	s->s_flags |= MS_NODIRATIME | MS_NOSUID | MS_NOEXEC;
 	s->s_blocksize = 1024;
 	s->s_blocksize_bits = 10;
