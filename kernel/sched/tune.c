866436e851a92b7038b1e8e76ebc216bf1b9c382
joshuous <joshuous@gmail.com>
2018-06-17 17:23:35 +0200

    sched/tune: Rename dynamic_boost_write() to dynamic_boost()
    This is to reduce confusion when we create a new dynamic_boost_write()
    function in future patches.
    
    Signed-off-by: joshuous <joshuous@gmail.com>

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -31,7 +31,7 @@ int schedtune_perf_threshold(void)
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
 static DEFINE_MUTEX(stune_boost_mutex);
 static struct schedtune *getSchedtune(char *st_name);
-static int dynamic_boost_write(struct schedtune *st, int boost);
+static int dynamic_boost(struct schedtune *st, int boost);
 #endif /* CONFIG_DYNAMIC_STUNE_BOOST */
 
 /* Performance Boost region (B) threshold params */
@@ -1233,7 +1233,7 @@ static struct schedtune *getSchedtune(char *st_name)
 	return NULL;
 }
 
-static int dynamic_boost_write(struct schedtune *st, int boost)
+static int dynamic_boost(struct schedtune *st, int boost)
 {
 	int ret;
 	/* Backup boost_default */
@@ -1259,7 +1259,7 @@ int do_stune_boost(char *st_name, int boost)
 
 	/* Boost if new value is greater than current */
 	if (boost > st->boost)
-		ret = dynamic_boost_write(st, boost);
+		ret = dynamic_boost(st, boost);
 
 	mutex_unlock(&stune_boost_mutex);
 
@@ -1275,7 +1275,7 @@ int reset_stune_boost(char *st_name)
 		return -EINVAL;
 
 	mutex_lock(&stune_boost_mutex);
-	ret = dynamic_boost_write(st, st->boost_default);
+	ret = dynamic_boost(st, st->boost_default);
 	mutex_unlock(&stune_boost_mutex);
 
 	return ret;
