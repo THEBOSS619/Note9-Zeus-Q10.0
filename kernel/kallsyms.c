99c0600bbbc63a6905f6369c1c6c3ca095719b07
Jan Beulich <JBeulich@suse.com>
2018-09-03 06:09:34 -0600

    kallsyms: reduce size a little on 64-bit
    Both kallsyms_num_syms and kallsyms_markers[] don't really need to use
    unsigned long as their (base) types; unsigned int fully suffices.
    
    Signed-off-by: Jan Beulich <jbeulich@suse.com>
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>

--- kernel/kallsyms.c
+++ kernel/kallsyms.c
@@ -48,7 +48,7 @@ extern const u8 kallsyms_names[] __weak;
  * Tell the compiler that the count isn't in the small data section if the arch
  * has one (eg: FRV).
  */
-extern const unsigned long kallsyms_num_syms
+extern const unsigned int kallsyms_num_syms
 __attribute__((weak, section(".rodata")));
 
 extern const unsigned long kallsyms_relative_base
@@ -57,7 +57,7 @@ __attribute__((weak, section(".rodata")));
 extern const u8 kallsyms_token_table[] __weak;
 extern const u16 kallsyms_token_index[] __weak;
 
-extern const unsigned long kallsyms_markers[] __weak;
+extern const unsigned int kallsyms_markers[] __weak;
 
 #ifdef CONFIG_SEC_DUMP_SUMMARY
 void sec_debug_set_kallsyms_info(struct sec_debug_ksyms *ksyms, int magic)
