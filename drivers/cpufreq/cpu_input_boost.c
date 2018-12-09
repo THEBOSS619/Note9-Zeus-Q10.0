79bbed8b4fcd7595fae3ec18e34c510df9caed17
kdrag0n <dragon@khronodragon.com>
2018-12-09 12:01:01 -0800

    cpu_input_boost: remove wake boost functionality
    This device wakes very fast without boost.
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -151,11 +151,6 @@ void cpu_input_boost_kick_max(unsigned int duration_ms)
 	__cpu_input_boost_kick_max(b, duration_ms);
 }
 
-void cpu_input_boost_kick_wake(void)
-{
-	cpu_input_boost_kick_max(CONFIG_WAKE_BOOST_DURATION_MS);
-}
-
 static void __cpu_input_boost_kick_general(struct boost_drv *b,
 	unsigned int duration_ms)
 {
@@ -298,7 +293,6 @@ static int fb_notifier_cb(struct notifier_block *nb,
 	/* Boost when the screen turns on and unboost when it turns off */
 	if (*blank == FB_BLANK_UNBLANK) {
 		set_boost_bit(b, SCREEN_AWAKE);
-		__cpu_input_boost_kick_max(b, CONFIG_WAKE_BOOST_DURATION_MS);
 	} else {
 		clear_boost_bit(b, SCREEN_AWAKE);
 		unboost_all_cpus(b);
--- include/linux/cpu_input_boost.h
+++ include/linux/cpu_input_boost.h
@@ -8,7 +8,6 @@
 #ifdef CONFIG_CPU_INPUT_BOOST
 void cpu_input_boost_kick(void);
 void cpu_input_boost_kick_max(unsigned int duration_ms);
-void cpu_input_boost_kick_wake(void);
 void cpu_input_boost_kick_general(unsigned int duration_ms);
 #else
 static inline void cpu_input_boost_kick(void)
@@ -17,7 +16,6 @@ static inline void cpu_input_boost_kick(void)
 static inline void cpu_input_boost_kick_max(unsigned int duration_ms)
 {
 }
-static inline void cpu_input_boost_kick_wake(void)
 static inline void cpu_input_boost_kick_general(unsigned int duration_ms)
 {
 }
