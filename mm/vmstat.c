92122ddec33c1ef7de4a37daabb20347b4846fe1
kdrag0n <dragon@khronodragon.com>
2018-12-28 15:23:14 -0800

    mm: keep zoneinfo when stripped procfs is enabled
    This is necessary for userspace lmkd to work properly.
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- mm/vmstat.c
+++ mm/vmstat.c
@@ -1804,8 +1804,8 @@ static int __init setup_vmstat(void)
 	if (!IS_ENABLED(CONFIG_PROC_STRIPPED)) {
 		proc_create("buddyinfo", S_IRUGO, NULL, &fragmentation_file_operations);
 		proc_create("pagetypeinfo", 0400, NULL, &pagetypeinfo_file_ops);
-		proc_create("zoneinfo", S_IRUGO, NULL, &proc_zoneinfo_file_operations);
 	}
+	proc_create("zoneinfo", S_IRUGO, NULL, &proc_zoneinfo_file_operations);
 	proc_create("vmstat", S_IRUGO, NULL, &proc_vmstat_file_operations);
 #endif
 	return 0;
