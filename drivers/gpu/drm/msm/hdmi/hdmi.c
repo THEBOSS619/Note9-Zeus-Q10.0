e037dca449606bbc6c9511fc560b7e6031b7c2d5
kdrag0n <dragon@khronodragon.com>
2018-12-27 23:14:04 -0800

    Merge branch 'freak07-caf'
    CAF commits (mostly memory leak fixes along with the removal of unused
    NSA Speck support) from freak07's kernel. (because I'm lazy)

--- drivers/gpu/drm/msm/hdmi/hdmi.c
+++ drivers/gpu/drm/msm/hdmi/hdmi.c
@@ -1,5 +1,9 @@
 /*
+<<<<<<< HEAD
  * Copyright (c) 2014 The Linux Foundation. All rights reserved.
+=======
+ * Copyright (c) 2014, 2016, 2018 The Linux Foundation. All rights reserved.
+>>>>>>> b0c3bbac1b16... Merge branch 'freak07-caf'
  * Copyright (C) 2013 Red Hat
  * Author: Rob Clark <robdclark@gmail.com>
  *
@@ -654,6 +658,7 @@ static struct platform_driver msm_hdmi_driver = {
 	.driver = {
 		.name = "hdmi_msm",
 		.of_match_table = msm_hdmi_dt_match,
+		.suppress_bind_attrs = true,
 	},
 };
 
