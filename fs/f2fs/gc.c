78943ce92a1f25d5300eedc78eb114653052052a
Park Ju Hyung <qkrwngud825@gmail.com>
2019-04-01 21:55:33 +0900

    f2fs: explicitly flush device upon foreground GC
    GC itself will produce some dirty segments.
    This will allow more GCs to be made upon rapid GC or urgent GC.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- fs/f2fs/gc.c
+++ fs/f2fs/gc.c
@@ -1407,6 +1407,7 @@ int f2fs_gc(struct f2fs_sb_info *sbi, bool sync,
 		.ilist = LIST_HEAD_INIT(gc_list.ilist),
 		.iroot = RADIX_TREE_INIT(GFP_NOFS),
 	};
+	struct super_block *sb = sbi->sb;
 	unsigned long long last_skipped = sbi->skipped_atomic_files[FG_GC];
 	unsigned long long first_skipped;
 	unsigned int skipped_round = 0, round = 0;
