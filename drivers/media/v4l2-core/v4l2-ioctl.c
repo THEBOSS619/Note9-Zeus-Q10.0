56f33fee5a9595c7976e000461f48514f9569980
Ian Macdonald <ian@caliban.org>
2019-06-24 18:18:35 +0200

    media: v4l2-ioctl: Zeroing extendedmode breaks Samsung super slow motion
    This is a partial reversion of 607a79adcb46.

--- drivers/media/v4l2-core/v4l2-ioctl.c
+++ drivers/media/v4l2-core/v4l2-ioctl.c
@@ -1966,12 +1966,10 @@ static int v4l_s_parm(const struct v4l2_ioctl_ops *ops,
 	if (V4L2_TYPE_IS_OUTPUT(p->type)) {
 		memset(p->parm.output.reserved, 0,
 		       sizeof(p->parm.output.reserved));
-		p->parm.output.extendedmode = 0;
 		p->parm.output.outputmode &= V4L2_MODE_HIGHQUALITY;
 	} else {
 		memset(p->parm.capture.reserved, 0,
 		       sizeof(p->parm.capture.reserved));
-		p->parm.capture.extendedmode = 0;
 		p->parm.capture.capturemode &= V4L2_MODE_HIGHQUALITY;
 	}
 	return ops->vidioc_s_parm(file, fh, p);
