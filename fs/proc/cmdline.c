0d88fa90f65ce8a9ec316e9bcd0ec41682163f3b
cocafe <cocafehj@gmail.com>
2019-06-29 11:25:00 +0800

    fs: proc: cmdline: restrict proper length
    Signed-off-by: cocafe <cocafehj@gmail.com>

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -24,37 +24,56 @@ static const struct file_operations cmdline_proc_fops = {
 	.release	= single_release,
 };
 
-static void remove_flag(char *cmd, const char *flag)
+static int remove_flag(const char *flag)
 {
-	char *start_addr, *end_addr;
+	char *start_flag, *end_flag, *next_flag;
+	char *last_char = new_command_line + COMMAND_LINE_SIZE;
+	size_t rest_len, flag_len;
+	int ret = 0;
 
 	/* Ensure all instances of a flag are removed */
-	while ((start_addr = strstr(cmd, flag))) {
-		end_addr = strchr(start_addr, ' ');
-		if (end_addr)
-			memmove(start_addr, end_addr + 1, strlen(end_addr));
-		else
-			*(start_addr - 1) = '\0';
+	while ((start_flag = strstr(new_command_line, flag))) {
+		end_flag = strchr(start_flag, ' ');
+
+		/* this may happend when copied cmdline is filled up fully */
+		if (end_flag > last_char)
+			end_flag = last_char;
+
+		if (end_flag) {
+			next_flag = end_flag + 1;
+			rest_len = (size_t)(last_char - end_flag);
+			flag_len = (size_t)(end_flag - start_flag);
+
+			memmove(start_flag, next_flag, rest_len);
+			memset(last_char - flag_len, '\0', flag_len);
+		} else {
+			memset(start_flag - 1, '\0', last_char - start_flag);
+		}
+
+		ret++;
 	}
+
+	return ret;
 }
 
-static void remove_safetynet_flags(char *cmd)
+static void remove_safetynet_flags(void)
 {
-	remove_flag(cmd, "androidboot.enable_dm_verity=");
-	remove_flag(cmd, "androidboot.secboot=");
-	remove_flag(cmd, "androidboot.verifiedbootstate=");
-	remove_flag(cmd, "androidboot.veritymode=");
+	remove_flag("androidboot.enable_dm_verity=");
+	remove_flag("androidboot.secboot=");
+	remove_flag("androidboot.verifiedbootstate=");
+	remove_flag("androidboot.veritymode=");
 }
 
 static int __init proc_cmdline_init(void)
 {
-	strcpy(new_command_line, saved_command_line);
+	memcpy(new_command_line, saved_command_line,
+		min((size_t)COMMAND_LINE_SIZE, strlen(saved_command_line)));
 
 	/*
 	 * Remove various flags from command line seen by userspace in order to
 	 * pass SafetyNet CTS check.
 	 */
-	remove_safetynet_flags(new_command_line);
+	remove_safetynet_flags();
 
 	proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
 	return 0;
