4dfac1b1b4446caec5da375b8d3692b21a1e275e
Pavankumar Kondeti <pkondeti@codeaurora.org>
2019-10-14 16:47:10 +0530

    sched: use the CPU true capacity while sorting the min/mid/max CPUs
    The current code uses the rq->cpu_capacity_orig while sorting
    the min/mid/max CPUs while rebuilding the scheduler domains. This
    rq->cpu_capacity_orig is subjected to changed when the scaling max
    frequency is clipped for that CPU's frequency domain. Since we don't
    recompute the min/mid/max CPUs when the capacity is changed, the
    sorting becomes incorrect later when the frequency limits are lifted.
    
    The task CPU selection algorithm depends heavily on min/mid/max CPUs
    and changing them on the fly results in incorrect task placement.
    Hence use the true capacity of CPUs while sorting the min/mid/max
    CPUs. This means that the sorting gets changed only when CPUs are
    hotplugged out, otherwise these reflect the correct topology
    all the time.
    
    The max_cpu_capacity struct in root domain also maintains the
    max CPU and its capacity. Since we now use true capacity, there
    is no need to compute this on the fly. So move this evaluation from
    the periodic load balancer to the scheduler domain rebuilding.
    
    The rq->cpu_capacity_orig is still subjected to change upon frequency
    or thermal limits. So we still identify the cases where tasks not
    fitting on the max/mid CPUs when the capacity is reduced.
    
    Change-Id: I42735accd079b2ece1eb58d1ebcf322d454e33a2
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    [Fixup minor merge error]
    Signed-off-by: Zachariah Kennedy <zkennedy87@gmail.com>
    Signed-off-by: celtare21 <celtare21@gmail.com>
    
    .

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -9306,10 +9306,6 @@ static void update_cpu_capacity(struct sched_domain *sd, int cpu)
 {
 	unsigned long capacity = arch_scale_cpu_capacity(sd, cpu);
 	struct sched_group *sdg = sd->groups;
-	struct max_cpu_capacity *mcc;
-	unsigned long max_capacity;
-	int max_cap_cpu;
-	unsigned long flags;
 	bool update = false;
 
 	cpu_rq(cpu)->cpu_capacity_orig = capacity;
@@ -9323,26 +9319,6 @@ static void update_cpu_capacity(struct sched_domain *sd, int cpu)
 	}
 	ehmp_update_overutilized(cpu, capacity);
 
-	mcc = &cpu_rq(cpu)->rd->max_cpu_capacity;
-
-	raw_spin_lock_irqsave(&mcc->lock, flags);
-	max_capacity = mcc->val;
-	max_cap_cpu = mcc->cpu;
-
-	if ((max_capacity > capacity && max_cap_cpu == cpu) ||
-	    (max_capacity < capacity)) {
-		mcc->val = capacity;
-		mcc->cpu = cpu;
-		ehmp_update_max_cpu_capacity(cpu, capacity);
-#ifdef CONFIG_SCHED_DEBUG
-		raw_spin_unlock_irqrestore(&mcc->lock, flags);
-		//pr_info("CPU%d: update max cpu_capacity %lu\n", cpu, capacity); *too spammy*
-		goto skip_unlock;
-#endif
-	}
-	raw_spin_unlock_irqrestore(&mcc->lock, flags);
-
-skip_unlock: __attribute__ ((unused));
 	capacity = scale_rt_capacity(cpu, capacity);
 
 	if (!capacity)
