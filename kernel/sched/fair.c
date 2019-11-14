b996de40b7ca7ac17701a22e2bf25e1caa878d4b
Viresh Kumar <viresh.kumar@linaro.org>
2019-11-14 16:19:27 +0530

    sched/fair: Make sched-idle CPU selection consistent throughout
    There are instances where we keep searching for an idle CPU despite
    already having a sched-idle CPU (in find_idlest_group_cpu(),
    select_idle_smt() and select_idle_cpu() and then there are places where
    we don't necessarily do that and return a sched-idle CPU as soon as we
    find one (in select_idle_sibling()). This looks a bit inconsistent and
    it may be worth having the same policy everywhere.
    
    On the other hand, choosing a sched-idle CPU over a idle one shall be
    beneficial from performance and power point of view as well, as we don't
    need to get the CPU online from a deep idle state which wastes quite a
    lot of time and energy and delays the scheduling of the newly woken up
    task.
    
    This patch tries to simplify code around sched-idle CPU selection and
    make it consistent throughout.
    
    Testing is done with the help of rt-app on hikey board (ARM64 octa-core,
    2 clusters, 0-3 and 4-7). The cpufreq governor was set to performance to
    avoid any side affects from CPU frequency. Following are the tests
    performed:
    
    Test 1: 1-cfs-task:
    
     A single SCHED_NORMAL task is pinned to CPU5 which runs for 2333 us
     out of 7777 us (so gives time for the cluster to go in deep idle
     state).
    
    Test 2: 1-cfs-1-idle-task:
    
     A single SCHED_NORMAL task is pinned on CPU5 and single SCHED_IDLE
     task is pinned on CPU6 (to make sure cluster 1 doesn't go in deep idle
     state).
    
    Test 3: 1-cfs-8-idle-task:
    
     A single SCHED_NORMAL task is pinned on CPU5 and eight SCHED_IDLE
     tasks are created which run forever (not pinned anywhere, so they run
     on all CPUs). Checked with kernelshark that as soon as NORMAL task
     sleeps, the SCHED_IDLE task starts running on CPU5.
    
    And here are the results on mean latency (in us), using the "st" tool.
    
      $ st 1-cfs-task/rt-app-cfs_thread-0.log
      N       min     max     sum     mean    stddev
      642     90      592     197180  307.134 109.906
    
      $ st 1-cfs-1-idle-task/rt-app-cfs_thread-0.log
      N       min     max     sum     mean    stddev
      642     67      311     113850  177.336 41.4251
    
      $ st 1-cfs-8-idle-task/rt-app-cfs_thread-0.log
      N       min     max     sum     mean    stddev
      643     29      173     41364   64.3297 13.2344
    
    The mean latency when we need to:
    
     - wakeup from deep idle state is 307 us.
     - wakeup from shallow idle state is 177 us.
     - preempt a SCHED_IDLE task is 64 us.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Link: https://lkml.kernel.org/r/b90cbcce608cef4e02a7bbfe178335f76d201bab.1573728344.git.viresh.kumar@linaro.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6632,7 +6632,7 @@ static int select_idle_cpu(struct task_struct *p, struct sched_domain *sd, int t
 	for_each_cpu_wrap(cpu, cpus, target) {
 		if (!--nr)
 			return -1;
-		if (idle_cpu(cpu))
+		if (idle_cpu(cpu)) || sched_idle_cpu(cpu)
 			break;
 	}
 
