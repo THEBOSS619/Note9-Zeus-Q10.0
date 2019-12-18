9c14a19f8d23ddd27f4a52bdaa9b16adf742e8a1
celtare21 <celtare21@gmail.com>
2019-12-18 11:24:44 +0000

    block,cfq: Disable logging if trace is not enabled
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- block/cfq-iosched.c
+++ block/cfq-iosched.c
@@ -664,8 +664,9 @@ static inline void cfqg_put(struct cfq_group *cfqg)
 
 #define cfq_log_cfqq(cfqd, cfqq, fmt, args...)	do {			\
 	char __pbuf[128];						\
-									\
-	blkg_path(cfqg_to_blkg((cfqq)->cfqg), __pbuf, sizeof(__pbuf));	\
+	blkg_path(cfqg_to_blkg((cfqq)->cfqg), __pbuf, sizeof(__pbuf));\
+	if (likely(!blk_trace_note_message_enabled((bfqd)->queue)))	\
+		break;								\
 	blk_add_trace_msg((cfqd)->queue, "cfq%d%c%c %s " fmt, (cfqq)->pid, \
 			cfq_cfqq_sync((cfqq)) ? 'S' : 'A',		\
 			cfqq_type((cfqq)) == SYNC_NOIDLE_WORKLOAD ? 'N' : ' ',\
@@ -674,7 +675,8 @@ static inline void cfqg_put(struct cfq_group *cfqg)
 
 #define cfq_log_cfqg(cfqd, cfqg, fmt, args...)	do {			\
 	char __pbuf[128];						\
-									\
+	if (likely(!blk_trace_note_message_enabled((bfqd)->queue)))	\
+		break;													\
 	blkg_path(cfqg_to_blkg(cfqg), __pbuf, sizeof(__pbuf));		\
 	blk_add_trace_msg((cfqd)->queue, "%s " fmt, __pbuf, ##args);	\
 } while (0)
