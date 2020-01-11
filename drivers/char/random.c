f2f793f1fcee7a1b610e86069f60da41fc245bd8
THEBOSS <ehabhani59@gmail.com>
2020-01-12 01:10:34 +0300

    Fix build

--- drivers/char/random.c
+++ drivers/char/random.c
@@ -853,7 +853,7 @@ static int crng_slow_load(const char *cp, size_t len)
 	unsigned long flags;
 	static unsigned char lfsr = 1;
 	unsigned char tmp;
-	unsigned int i, max = CHACHA20_KEY_SIZE;
+	unsigned int i, max = CHACHA_KEY_SIZE;
 	const char *src_buf = cp;
 	char *dest_buf = (char *) &primary_crng.state[4];
 
@@ -871,8 +871,8 @@ static int crng_slow_load(const char *cp, size_t len)
 		lfsr >>= 1;
 		if (tmp & 1)
 			lfsr ^= 0xE1;
-		tmp = dest_buf[i % CHACHA20_KEY_SIZE];
-		dest_buf[i % CHACHA20_KEY_SIZE] ^= src_buf[i % len] ^ lfsr;
+		tmp = dest_buf[i % CHACHA_KEY_SIZE];
+		dest_buf[i % CHACHA_KEY_SIZE] ^= src_buf[i % len] ^ lfsr;
 		lfsr += (tmp << 3) | (tmp >> 5);
 	}
 	spin_unlock_irqrestore(&primary_crng.lock, flags);
