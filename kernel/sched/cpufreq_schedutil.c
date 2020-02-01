516e7ab3037e38709ff6971d9eda590a72c43a9f
THEBOSS <ehabhani59@gmail.com>
2020-02-01 21:18:24 +0300

    Fine tuning boostings overall

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -825,8 +825,8 @@ static int sugov_init(struct cpufreq_policy *policy)
 	} else {
 		unsigned int lat;
 
-                tunables->up_rate_limit_us = 150;
-                tunables->down_rate_limit_us = 150;
+                tunables->up_rate_limit_us = UP_LATENCY_MULTIPLIER;
+                tunables->down_rate_limit_us = DOWN_LATENCY_MULTIPLIER;
 		lat = policy->cpuinfo.transition_latency / NSEC_PER_USEC;
 		if (lat) {
                         tunables->up_rate_limit_us *= lat;
