fb149d0f50846b7eaa1717fe3421b2dc2cea2f2c
Diep Quynh <remilia.1505@gmail.com>
2020-02-20 02:10:46 +0700

    wakelock_blocker: Generate default wakelocks list
    These are grepped as per services having largest time_while_screen_off
    in wakeup_sources located in /sys/kernel/debug/wakeup_sources
    
    The wakelocks are tested carefully before going to block list to make
    sure no critical hardware malfunctions were made after blocking
    
    Signed-off-by: Diep Quynh <remilia.1505@gmail.com>

--- drivers/base/power/boeffla_wl_blocker.h
+++ drivers/base/power/boeffla_wl_blocker.h
@@ -16,7 +16,7 @@
 
 #define BOEFFLA_WL_BLOCKER_VERSION	"1.1.0"
 
-#define LIST_WL_DEFAULT			"wlan_txfl_wake;ssp_comm_wake_lock;GPSD;ssp_wake_lock;tsp_wakelock;umts_ipc0;umts_ipc1;wlan_rx_wake;wlan_wake;NETLINK;abox;bbd_wake_lock;mmc0_detect"
+#define LIST_WL_DEFAULT			"16030000.decon_f;wlan_txfl_wake;ssp_comm_wake_lock;GPSD;ssp_wake_lock;tsp_wakelock;umts_ipc0;umts_ipc1;wlan_rx_wake;wlan_wake;NETLINK;abox;bbd_wake_lock;mmc0_detect"
 
 #define LENGTH_LIST_WL			2048
 #define LENGTH_LIST_WL_DEFAULT		150
