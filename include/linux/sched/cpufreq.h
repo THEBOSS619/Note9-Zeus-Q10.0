c1f8d1a1f7dbbaae3fccb9ee1dae1d85b26c66cb
PrimoDev23 <lexx.ps2711@gmail.com>
2020-02-21 09:10:20 +0100

    [TEST]cpufreq: Update frequency calculation a bit
    This is according to tytydraco's https://github.com/tytydraco/kernel_oneplus_sm8150/commit/8fcff168e1565315a852036a87eaad93a7d61035
    
    I calculated the capacity that is needed for max frequency with the following method
    
    needed util for max freq = (max_freq * max_cap) / (max_freq + (max_freq >> 2))
    
    LP:
    needed util for max freq = (1785600 * 280) / (1785600 + 446400) = 224
    utilization = 224/280 = 0.8
    
    PERF:
    needed util for max freq = (2419200 * 871) / (2419200 + 604800) = 696.8
    utilization = 696.8/871 = 0.8
    
    PRIME:
    needed util for max freq = (2841600 * 1024) / (2841600 + 710400) = 819.2
    utilization = 819.2/1024 = 0.8
    
    This results in around 80% util needed for the max freq
    In this case we would waste power when util isn't even near max capacity
    
    To correct this I tried with a right shift of 3
    
    needed util for max freq = (max_freq * max_cap) / (max_freq + (max_freq >> 3))
    
    LP:
    needed util for max freq = (1785600 * 280) / (1785600 + 223200) = 248.88888889
    utilization = 248.88888889/280 = 0.8888
    
    PERF:
    needed util for max freq = (2419200 * 871) / (2419200 + 302400) = 774.22222222
    utilization = 774.22222222/871 = 0.8888
    
    PRIME:
    needed util for max freq = (2841600 * 1024) / (2841600 + 355200) = 910.22222222
    utilization = 910.22222222/1024 = 0.8888
    
    Which results in around 90% util needed for max freq
    This should be a bit better in terms of frequency selection
    
    The same happens when using a right shit of 4 and so on
    
    Shift   | utilization for max freq
    2	| 0.8
    3	| 0.89
    4	| 0.94
    5	| 0.97
    6	| 0.98
    
    Let's use a right shift of 3 for now and don't be too aggressive.
    
    Since we are also using a high minfreq on all clusters we won't have any issues
    with too low frequencies.
    
    Just to mention, our prime cluster runs on like < 10% util all the time which would
    result in a pretty low frequency.
    
    So this change won't do much to low frequency selection cause of our higher minfreqs
    instead it will influence the high frequency selection.
    
    Signed-off-by: PrimoDev23 <lexx.ps2711@gmail.com>

--- include/linux/sched/cpufreq.h
+++ include/linux/sched/cpufreq.h
@@ -29,7 +29,7 @@ void cpufreq_remove_update_util_hook(int cpu);
 static inline unsigned long map_util_freq(unsigned long util,
 					unsigned long freq, unsigned long cap)
 {
-	return (freq + (freq >> 2)) * int_sqrt(util * 100 / cap) / 10;
+	return (freq + (freq >> 3)) * util / cap;
 }
 
 bool cpufreq_this_cpu_can_update(struct cpufreq_policy *policy);
