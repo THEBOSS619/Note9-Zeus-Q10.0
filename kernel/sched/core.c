a3e3776da829fbb13fd788e2954238bf3ccfea96
Tyler Nijmeh <tylernij@gmail.com>
2020-02-21 10:39:44 -0800

    sched: Allow realtime tasks to consume entire sched periods
    If the scenario is right, we can run realtime tasks for 5% longer. This
    also disables lockup protection from unhandled realtime tasks.
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -169,7 +169,7 @@ __read_mostly int scheduler_running;
  * part of the period that we allow rt tasks to run in us.
  * default: 0.95s
  */
-int sysctl_sched_rt_runtime = 950000;
+int sysctl_sched_rt_runtime = -1;
 
 /*
  * Number of sched_yield calls that result in a thread yielding
