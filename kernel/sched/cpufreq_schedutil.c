f8734a0e6348e3a072d9c52fc232138ab2db73fe
THEBOSS <ehabhani59@gmail.com>
2020-02-24 18:46:34 +0300

    Optimizing Note 9 Kernel Internal Operation
    - Using PELT 16ms Finally [We have optimized kernel power efficiency quite enough to go from 32ms to 16ms]
    - OC MIF Bus to 2093Mhz rather than 1794Mhz [Same reason as above]
    - Tuned Mali GPU scalings
    - Applied some values extracted from S10 Exynos devices
    - Tweaked Booting value, Delay time, Frequency Info parameters
    - Tweaked out CPUSET
    - Going from 250Hz to 300Hz timer frequency according to Google's standard
    - Made Schedutil up/down rate limit to 125 rather than LATENCY_MULTIPLIER
    
    .

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -828,8 +828,8 @@ static int sugov_init(struct cpufreq_policy *policy)
 	} else {
 		unsigned int lat;
 
-                tunables->up_rate_limit_us = UP_LATENCY_MULTIPLIER;
-                tunables->down_rate_limit_us = DOWN_LATENCY_MULTIPLIER;
+                tunables->up_rate_limit_us = 125;
+                tunables->down_rate_limit_us = 125;
 		lat = policy->cpuinfo.transition_latency / NSEC_PER_USEC;
 		if (lat) {
                         tunables->up_rate_limit_us *= lat;
