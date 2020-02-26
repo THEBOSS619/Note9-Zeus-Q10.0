32361f4c046ac24678855f871359d1c85d728add
THEBOSS <ehabhani59@gmail.com>
2020-02-26 18:07:06 +0300

    Revert "zswap: adjust max_pool_percent to 30"
    This reverts commit 869baf83369297f1b11ee0a7e2cd427f8f7fe05c.

--- mm/zswap.c
+++ mm/zswap.c
@@ -123,7 +123,7 @@ static struct kernel_param_ops zswap_zpool_param_ops = {
 module_param_cb(zpool, &zswap_zpool_param_ops, &zswap_zpool_type, 0644);
 
 /* The maximum percentage of memory that the compressed pool can occupy */
-static unsigned int zswap_max_pool_percent = 30;
+static unsigned int zswap_max_pool_percent = 50;
 module_param_named(max_pool_percent, zswap_max_pool_percent, uint, 0644);
 
 /* Enable/disable handling same-value filled pages (enabled by default) */
