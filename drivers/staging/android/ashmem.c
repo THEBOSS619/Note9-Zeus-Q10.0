1f4dc8dedc138e959c1df0c8e4cda107d987f1ea
THEBOSS <ehabhani59@gmail.com>
2020-02-29 03:41:04 +0300

    Fix 4.9.215 merge
    due to this [https://github.com/THEBOSS619/Note9-Zeus-Q10.0/commit/4eef2d200c84c2bd3105ed205051e63d9cdddc78] commit being used on this kernel...

--- drivers/staging/android/ashmem.c
+++ drivers/staging/android/ashmem.c
@@ -349,25 +349,6 @@ static inline vm_flags_t calc_vm_may_flags(unsigned long prot)
 	       _calc_vm_trans(prot, PROT_EXEC,  VM_MAYEXEC);
 }
 
-static int ashmem_file_setup(struct ashmem_area *asma,
-			     struct vm_area_struct *vma)
-{
-	char name[ASHMEM_FULL_NAME_LEN] = ASHMEM_NAME_DEF;
-	struct file *vmfile;
-
-	spin_lock(&asma->name_lock);
-	if (asma->name[ASHMEM_NAME_PREFIX_LEN] != '\0')
-		strcpy(name, asma->name);
-	spin_unlock(&asma->name_lock);
-
-	/* ... and allocate the backing shmem file */
-	vmfile = shmem_file_setup(name, asma->size, vma->vm_flags);
-	if (IS_ERR(vmfile))
-		return PTR_ERR(vmfile);
-	vmfile->f_mode |= FMODE_LSEEK;
-	WRITE_ONCE(asma->file, vmfile);
-	return 0;
-}
 
 static int ashmem_vmfile_mmap(struct file *file, struct vm_area_struct *vma)
 {
@@ -383,9 +364,43 @@ ashmem_vmfile_get_unmapped_area(struct file *file, unsigned long addr,
 	return current->mm->get_unmapped_area(file, addr, len, pgoff, flags);
 }
 
+static int ashmem_file_setup(struct ashmem_area *asma,
+			     struct vm_area_struct *vma)
+{
+	char name[ASHMEM_FULL_NAME_LEN] = ASHMEM_NAME_DEF;
+	static struct file_operations vmfile_fops;
+	struct file *vmfile;
+
+	spin_lock(&asma->name_lock);
+	if (asma->name[ASHMEM_NAME_PREFIX_LEN] != '\0')
+		strcpy(name, asma->name);
+	spin_unlock(&asma->name_lock);
+
+	/* ... and allocate the backing shmem file */
+	vmfile = shmem_file_setup(name, asma->size, vma->vm_flags);
+	if (IS_ERR(vmfile))
+		return PTR_ERR(vmfile);
+	vmfile->f_mode |= FMODE_LSEEK;
+		/*
+		 * override mmap operation of the vmfile so that it can't be
+		 * remapped which would lead to creation of a new vma with no
+		 * asma permission checks. Have to override get_unmapped_area
+		 * as well to prevent VM_BUG_ON check for f_ops modification.
+		 */
+		if (!vmfile_fops.mmap) {
+			vmfile_fops = *vmfile->f_op;
+			vmfile_fops.mmap = ashmem_vmfile_mmap;
+			vmfile_fops.get_unmapped_area =
+					ashmem_vmfile_get_unmapped_area;
+		}
+		vmfile->f_op = &vmfile_fops;
+
+	WRITE_ONCE(asma->file, vmfile);
+	return 0;
+}
+
 static int ashmem_mmap(struct file *file, struct vm_area_struct *vma)
 {
-	static struct file_operations vmfile_fops;
 	struct ashmem_area *asma = file->private_data;
 	unsigned long prot_mask;
 	size_t size;
@@ -419,20 +434,6 @@ static int ashmem_mmap(struct file *file, struct vm_area_struct *vma)
 				asma->file_is_setup = true;
 		}
 		rt_mutex_unlock(&mmap_lock);
-
-		/*
-		 * override mmap operation of the vmfile so that it can't be
-		 * remapped which would lead to creation of a new vma with no
-		 * asma permission checks. Have to override get_unmapped_area
-		 * as well to prevent VM_BUG_ON check for f_ops modification.
-		 */
-		if (!vmfile_fops.mmap) {
-			vmfile_fops = *vmfile->f_op;
-			vmfile_fops.mmap = ashmem_vmfile_mmap;
-			vmfile_fops.get_unmapped_area =
-					ashmem_vmfile_get_unmapped_area;
-		}
-		vmfile->f_op = &vmfile_fops;
 
 		if (do_setup && ret)
 			return ret;
