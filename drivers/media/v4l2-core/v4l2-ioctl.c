0ctobot/neutrino_kernel_oneplus_sdm845

[FIXUP] media: v4l2-ioctl: Use a larger on-stack video copy buffer
We have a 16 KiB stack; a buffer of 1 KiB works perfectly fine in place
of a small 128-byte buffer. This avoids dynamic memory allocation more
often, improving performance.

Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>

drivers/media/v4l2-core/v4l2-ioctl.c
@@ -2902,7 +2902,8 @@ long
 video_usercopy(struct file *file, unsigned int cmd, unsigned long arg,
 	       v4l2_kioctl func)
 {
-	char	sbuf[SZ_1K];
+	char	sbuf[SZ_4K] __aligned(8);
+	char    mbuf_onstack[SZ_512] __aligned(8);
 	void    *mbuf = NULL;
 	void	*parg = (void *)arg;
 	long	err  = -EINVAL;
@@ -2964,10 +2965,14 @@ video_usercopy(struct file *file, unsigned int cmd, unsigned long arg,
 		 * array) fits into sbuf (so that mbuf will still remain
 		 * unused up to here).
 		 */
-		mbuf = kmalloc(array_size, GFP_KERNEL);
-		err = -ENOMEM;
-		if (NULL == mbuf)
-			goto out_array_args;
+		if (array_size <= ARRAY_SIZE(mbuf_onstack)) {
+			mbuf = mbuf_onstack;
+		} else {
+			mbuf = kmalloc(array_size, GFP_KERNEL);
+			err = -ENOMEM;
+			if (NULL == mbuf)
+				goto out_array_args;
+		}
 		err = -EFAULT;
 		if (copy_from_user(mbuf, user_ptr, array_size))
 			goto out_array_args;
@@ -3010,7 +3015,8 @@ out_array_args:
 	}
 
 out:
-	kfree(mbuf);
+	if (mbuf != mbuf_onstack)
+		kfree(mbuf);
 	return err;
 }
 EXPORT_SYMBOL(video_usercopy);

THEBOSS619/Note9-Zeus-Q10.0

drivers/media/v4l2-core/v4l2-ioctl.c
@@ -2823,7 +2823,8 @@ long
 video_usercopy(struct file *file, unsigned int cmd, unsigned long arg,
 	       v4l2_kioctl func)
 {
-	char	sbuf[SZ_1K];
+	char	sbuf[SZ_4K] __aligned(8);
+	char    mbuf_onstack[SZ_512] __aligned(8);
 	void    *mbuf = NULL;
 	void	*parg = (void *)arg;
 	long	err  = -EINVAL;
@@ -2885,10 +2886,14 @@ video_usercopy(struct file *file, unsigned int cmd, unsigned long arg,
 		 * array) fits into sbuf (so that mbuf will still remain
 		 * unused up to here).
 		 */
-		mbuf = kvmalloc(array_size, GFP_KERNEL);
-		err = -ENOMEM;
-		if (NULL == mbuf)
-			goto out_array_args;
+		if (array_size <= ARRAY_SIZE(mbuf_onstack)) {
+			mbuf = mbuf_onstack;
+		} else {
+			mbuf = kvmalloc(array_size, GFP_KERNEL);
+			err = -ENOMEM;
+			if (NULL == mbuf)
+				goto out_array_args;
+		}
 		err = -EFAULT;
 		if (copy_from_user(mbuf, user_ptr, array_size))
 			goto out_array_args;
@@ -2931,7 +2936,8 @@ out_array_args:
 	}
 
 out:
-	kvfree(mbuf);
+	if (mbuf != mbuf_onstack)
+		kvfree(mbuf);
 	return err;
 }
 EXPORT_SYMBOL(video_usercopy);