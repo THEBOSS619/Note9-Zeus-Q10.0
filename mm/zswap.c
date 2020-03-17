d5e36d489af83c48bf84c50ab90af7c7bef5bd68
Park Ju Hyung <qkrwngud825@gmail.com>
2020-03-18 03:35:45 +0900

    zswap: add bp hints to memory allocations
    These are called frequently.
    
    Add bp hints to avoid pipeline flushes.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- mm/zswap.c
+++ mm/zswap.c
@@ -304,7 +304,7 @@ static struct zswap_entry *zswap_entry_cache_alloc(gfp_t gfp)
 {
 	struct zswap_entry *entry;
 	entry = kmem_cache_alloc(zswap_entry_cache, gfp);
-	if (!entry)
+	if (unlikely(!entry))
 		return NULL;
 	entry->refcount = 1;
 #ifdef CONFIG_ZSWAP_SAME_PAGE_SHARING
@@ -1218,7 +1218,7 @@ static int zswap_frontswap_store(unsigned type, pgoff_t offset,
 
 	/* allocate entry */
 	entry = zswap_entry_cache_alloc(GFP_KERNEL);
-	if (!entry) {
+	if (unlikely(!entry)) {
 		zswap_reject_kmemcache_fail++;
 		ret = -ENOMEM;
 		goto reject;
