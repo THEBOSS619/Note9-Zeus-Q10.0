512c3ba5bf56083b019e10f5acc1c8ab67c6d33e
xNombre <kartapolska@gmail.com>
2020-03-24 00:19:27 +0100

    block: Completely disable readahead
    ...and restrict if from being changed by userspace.
    
    Remove deprecated flag while at it.
    
    PCMark Storage bench showed ~130 points increase in performance.
    It will also save much power.
    
    @THEBOSS619: To Include more evidences...
    
    [*] https://twitter.com/arter97/status/1003054292897370112
    [*] https://www.semanticscholar.org/paper/Revisiting-read-ahead-efficiency-for-raw-NAND-flash-Olivier-Boukhobza/297f3d189dcd9b099f8f6b5fbc7e304db395a683
    
    Signed-off-by: xNombre <kartapolska@gmail.com>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -2349,7 +2349,7 @@ int write_one_page(struct page *page, int wait);
 void task_dirty_inc(struct task_struct *tsk);
 
 /* readahead.c */
-#define VM_MAX_READAHEAD	64	/* kbytes */
+#define VM_MAX_READAHEAD	0	/* kbytes */
 #define VM_MIN_READAHEAD	0	/* kbytes (includes current page) */
 
 int force_page_cache_readahead(struct address_space *mapping, struct file *filp,
