6c0eee1b569619be514d64899c38f4e23bdb8049
THEBOSS <ehabhani59@gmail.com>
2020-04-08 21:58:19 +0300

    IMPORT SM-N960F_QQ_OSRC_N960FXXU5DTCA

--- mm/vmpressure.c
+++ mm/vmpressure.c
@@ -33,7 +33,7 @@
  * essence, they are percents: the higher the value, the more number
  * unsuccessful reclaims there were.
  */
-static const unsigned int vmpressure_level_med = 60;
+static const unsigned int vmpressure_level_med = CONFIG_VMPRESSURE_LEVEL_MED;
 static const unsigned int vmpressure_level_critical = 95;
 
 static unsigned long vmpressure_scale_max = 100;
