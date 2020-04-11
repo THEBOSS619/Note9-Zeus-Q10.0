371b2a33f06bc189a14a0f0e74d3565f147f0e09
THEBOSS <ehabhani59@gmail.com>
2020-04-11 22:08:18 +0300

    block: Reduce block overhead by reducing polling it

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -591,8 +591,7 @@ struct request_queue {
 				 (1 << QUEUE_FLAG_SAME_COMP))
 
 #define QUEUE_FLAG_MQ_DEFAULT	((1 << QUEUE_FLAG_STACKABLE)	|	\
-				 (1 << QUEUE_FLAG_SAME_COMP)	|	\
-				 (1 << QUEUE_FLAG_POLL))
+				 (1 << QUEUE_FLAG_SAME_COMP)
 
 static inline void queue_lockdep_assert_held(struct request_queue *q)
 {
