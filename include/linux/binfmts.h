45c09b9017cdd280907b05bdcdb07d62a623293f
THEBOSS <ehabhani59@gmail.com>
2020-04-21 06:21:13 +0300

    A Couple Of Changes

--- include/linux/binfmts.h
+++ include/linux/binfmts.h
@@ -130,7 +130,10 @@ static inline bool task_is_booster(struct task_struct *tsk)
 	char comm[sizeof(tsk->comm)];
 
 	get_task_comm(comm, tsk);
-	return NULL;
+	return !strcmp(comm, "init") || !strcmp(comm, "NodeLooperThrea") ||
+	       !strcmp(comm, "power@1.3-servi") ||
+	       !strcmp(comm, "perf@1.0-servic") ||
+	       !strcmp(comm, "init.qcom.post_");
 }
 
 #endif /* _LINUX_BINFMTS_H */
--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -898,8 +898,8 @@ static int sugov_init(struct cpufreq_policy *policy)
 		goto stop_kthread;
 	}
 
-	tunables->up_rate_limit_us = 5000;
-	tunables->down_rate_limit_us = 5000;
+	tunables->up_rate_limit_us = 500;
+	tunables->down_rate_limit_us = 20000;
 
 	policy->governor_data = sg_policy;
 	sg_policy->tunables = tunables;
