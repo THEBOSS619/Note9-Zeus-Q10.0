47d8387a3b4832fdf96800ad6bf960331ce45cd8
THEBOSS <ehabhani59@gmail.com>
2020-05-03 23:27:56 +0300

    Revert "kernel: Only expose su when daemon is running"
    This reverts commit 65dbd24b833f3f0b2fb1cd5079924965f127e23d.

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -61,12 +61,6 @@ struct sched_param {
 
 #include <asm/processor.h>
 
-int  su_instances(void);
-bool su_running(void);
-bool su_visible(void);
-void su_exec(void);
-void su_exit(void);
-
 #define SCHED_ATTR_SIZE_VER0	48	/* sizeof first published struct */
 
 /*
@@ -2647,7 +2641,7 @@ extern void thread_group_cputime_adjusted(struct task_struct *p, u64 *ut, u64 *s
 #define PF_MUTEX_TESTER	0x20000000	/* Thread belongs to the rt mutex tester */
 #define PF_FREEZER_SKIP	0x40000000	/* Freezer should not count it as freezable */
 #define PF_SUSPEND_TASK 0x80000000      /* this thread called freeze_processes and should not be frozen */
-#define PF_SU			0x10000000      /* task is su */
+
 /*
  * Only the _current_ task can read/write to tsk->flags, but other
  * tasks can access tsk->flags in readonly mode for example
